557aa3c4bb99af8e7da277693e2062abc82dd37f
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-09 03:08:22 +0300

    Create FIRST_HW.java

---
+++ UNIVERSITY PROJECTS/FIRST_HW.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    // FIFTH TASK
+    double x=(pow(7));
+    double y=(pow(5));
+    System.out.println(Math.sqrt(x+y));
+    //SIXTH TASK
+    
+  }
+  public static int pow(int value) {
+    int powValue= 2;
+    return (int) Math.pow(value, powValue);
+  }
+}
+
+
+
