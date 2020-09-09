dd9609a4ebc81f837bcc6563dad1d3fde385bd4f
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-09 03:10:53 +0300

    Update FIRST_HW.java

--- UNIVERSITY PROJECTS/FIRST_HW.java
+++ UNIVERSITY PROJECTS/FIRST_HW.java
@@ -1,14 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello world!"); // ТУТ ИДЕТ ЧИСТО ПРОВЕРКА, ШО ВСЕ РОБИТ XD
     // FIFTH TASK
-    double x=(pow(7));
-    double y=(pow(5));
-    System.out.println(Math.sqrt(x+y));
+    double x=(pow(7)); // ПОЛУЧАЕМ КВАДРАТ 7 
+    double y=(pow(5)); // А ТУТ 5
+    System.out.println(Math.sqrt(x+y)); // ТУТ Я РЕШИЛ ВОЗНЕСТИ В КОРЕНЬ ЧИСЛО БЕЗ МЕТОДА ))00))
     //SIXTH TASK
     
   }
-  public static int pow(int value) {
+  public static int pow(int value) { // ЭТОТ МЕТОД ОТВЕЧАЕТ ЗА ВОЗВЕДЕНИЕ ЧИСЕЛ В СТЕПЕНЬ
     int powValue= 2;
     return (int) Math.pow(value, powValue);
   }
