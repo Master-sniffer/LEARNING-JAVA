a9d713b9ce6fa8979d76180fee41f154528baefa
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-14 23:13:47 +0300

    Create MatrixCreate.java

---
+++ UNIVERSITY TASKS/OFFLINE TASKS/First/MatrixCreate.java
@@ -0,0 +1,21 @@
+public class MatrixCreate {
+  //int [] [] matrix1 = new int [20][30];
+  public int [][] matrix1;
+  
+  public MatrixCreate(int row, int col){ // КОНСТРУКТОР КЛАССА
+  
+  }
+
+  public void NUlMatrix(){ // void - Этот метод нужен, если мы не планируем что-либо возвращать (остальные спецификаторы , к прмимеру int, будут требовать возвращение)   
+  }
+  public int Nu(){ // ТУТ Вместо void стоит int (и надо теперь будет обязательно возвращать)
+    int a =2;
+    return a;
+  }
+
+  public void SetMatrix(int row, int col){
+    this.matrix1=matrix1[][];
+
+  }
+
+}
