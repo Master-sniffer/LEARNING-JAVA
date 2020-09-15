3dca634054d15bc6586a5e77d87e1b04f83703a9
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-15 14:59:34 +0300

    Update Main.java

--- UNIVERSITY TASKS/HW TASKS/Second/Main.java
+++ UNIVERSITY TASKS/HW TASKS/Second/Main.java
@@ -1,1 +1,43 @@
+import java.util.Random; // ИМПОРТИРУЕМ МОДУЛЬ РАНДОМА
 
+class Main{
+  public static void main(String[] args){
+  first(); //FIRST TASK
+  }
+
+
+  public static void first(){
+    int[][] matrix0 = new int[10][5];
+    int[][] matrix01 = new int[10][5];
+    int n = 10;
+    int k = 5; 
+
+    Random rand = new Random();// ЭТО ПОМОЖЕТ НАМ СОЗДАВАТЬ РАНДОМНЫЕ ЧИСЛА
+
+    for (int i =0; i < n; i++){
+      for (int b=0; b < k; b++){
+          matrix0[i][b]=rand.nextInt(100); // идет рандомное число до 100 в матрицу
+          matrix01[i][b]=rand.nextInt(100); // Идет рандомное число до 100 в матрицу
+      }
+    }
+    //System.out.println(matrix0); // Почему тут видна фигня ? Всё просто ! У нас указывается ссылка на элемент, а не сам элемент. Чтобы вызвать нормальный элемент - возьмите и напишите (матрица[строка][колонка])
+
+    // УЗНАТЬ, КАК ПОЛУЧАТЬ ЭЛЕМЕНТ ВО ВСЕМ КЛАССЕ
+    
+    int[][] result = new int[10][5];
+    for (int i =0; i < n; i++){
+      for (int b=0; b < k; b++){
+        result[i][b]=matrix0[i][b]-matrix01[i][b];
+      }
+    }
+
+    // ПЕЧАТАЕМ МАТРИЦУ
+    for (int i = 0; i < result.length; i++) {
+        for (int j = 0; j < result[i].length; j++) {
+            System.out.print(result[i][j] + " ");
+        }
+        System.out.println();
+    }
+  }
+  
+}
