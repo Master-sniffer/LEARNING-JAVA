1f4144acb0b57ac6c2c37e337b48aa5298fd29b3
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-16 13:47:20 +0300

    Update Main.java

--- UNIVERSITY TASKS/HW TASKS/Second/Main.java
+++ UNIVERSITY TASKS/HW TASKS/Second/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;  // НУЖЕН ДЛЯ ПОЛЬЗОВАТЕЛЬСКОГО ВВОДА
 import java.util.Random; // ИМПОРТИРУЕМ МОДУЛЬ РАНДОМА
 
 class Main{
@@ -12,6 +13,7 @@ class Main{
     int n = 10;
     int k = 5; 
 
+    Scanner sc= new Scanner(System.in); // ЭТО ПОМОЖЕТ НАМ ПОЛУЧАТЬ ЗНАЧЕНИЯ ОТ ЮЗВЕРЕЙ
     Random rand = new Random();// ЭТО ПОМОЖЕТ НАМ СОЗДАВАТЬ РАНДОМНЫЕ ЧИСЛА
 
     for (int i =0; i < n; i++){
@@ -34,24 +36,27 @@ class Main{
     }
     System.out.println("ПЕЧАТАЕМ МАТРИЦУ ВЫЧИТАНИЯ\n\n");
     // ПЕЧАТАЕМ МАТРИЦУ ВЫЧИТАНИЯ
-    for (int i = 0; i < result.length; i++) {
-        for (int j = 0; j < result[i].length; j++) {
-            System.out.print(result[i][j] + " ");
-        }
-        System.out.println();
-                                            } 
-
+    printa(result1);
 
     System.out.println("\n\nПЕЧАТАЕМ МАТРИЦУ СЛОЖЕНИЯ\n\n");
+    printa(result);
+    
+    System.out.println("\nENTER THE NUMBER");
+    int a= sc.nextInt();
 
+    for (int i =0; i < n; i++){
+      for (int b=0; b < k; b++){
+          matrix0[i][b]=rand.nextInt(100);
+      }}
+    
 
-    // ПЕЧАТАЕМ МАТРИЦУ СЛОЖЕНИЯ
+  }
+  public static void printa(int[][] result1){
     for (int i = 0; i < result1.length; i++) {
         for (int j = 0; j < result1[i].length; j++) {
             System.out.print(result1[i][j] + " ");
         }
         System.out.println();
-                                            } 
-  }
+                                            }} 
   
 }
