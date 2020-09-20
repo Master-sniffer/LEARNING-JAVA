ab258885a8efdb3bfba93b96621a1fd63872a869
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-20 21:18:26 +0300

    Create Main.java

---
+++ UNIVERSITY TASKS/HW TASKS/Second/Third_And_Fourth_TASK/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("AE, MA BOI\n");
+  }
+}
+
+public abstract class THIRD_TASK{
+
+  public abstract void display(); // КАК ГОВОРИЛ МОЙ ДЕД, ЕСЛИ В ТВОЕМ АБСТРАКТНОМ КЛАССЕ НЕТ АБСТРАКТНЫХ МЕТОДОВ , ТО ХЕРНЯ ЭТО , А НЕ АБСТРАКТНЫЙ КЛАСС
+}
