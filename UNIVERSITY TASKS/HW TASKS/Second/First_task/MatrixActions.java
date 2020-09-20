6e0b9f4ac21ad30d3eacda473ca12f806bdd0254
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-20 03:08:19 +0300

    Create MatrixActions.java

---
+++ UNIVERSITY TASKS/HW TASKS/Second/First_task/MatrixActions.java
@@ -0,0 +1,25 @@
+import java.util.Random; // ИМПОРТИРУЕМ МОДУЛЬ РАНДОМА
+class MatrixActions {
+
+  // МЕТОД ДЛЯ ПЕЧАТИ МАТРИЦЫ
+  public static void printa(int[][] result1){
+    for (int i = 0; i < result1.length; i++) {
+        for (int j = 0; j < result1[i].length; j++) {
+            System.out.print(result1[i][j] + " ");
+        }
+        System.out.println();}} 
+  // ВОТ И ВЕСЬ МЕТОД ))0)))
+
+  public int[][] create( int a, int b){
+
+    Random rand = new Random();
+    int[][] matrix = new int[a][b];
+    for (int i = 0; i < a; i++){
+      for (int c= 0; c < b; c++){
+          matrix[i][c]=rand.nextInt(100); // идет рандомное число до 100 в матрицу
+      }
+    }
+    return matrix;
+  }
+
+}
