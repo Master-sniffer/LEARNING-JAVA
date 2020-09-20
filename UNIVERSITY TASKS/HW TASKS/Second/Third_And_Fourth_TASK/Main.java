d32b10ead0391487f67fe90833857b56a31965cd
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-21 01:33:16 +0300

    Update Main.java

--- UNIVERSITY TASKS/HW TASKS/Second/Third_And_Fourth_TASK/Main.java
+++ UNIVERSITY TASKS/HW TASKS/Second/Third_And_Fourth_TASK/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.ArrayList; 
 
 class Main {
 
@@ -11,18 +12,26 @@ class Main {
     this.N=N;
     System.out.println("FUNNY , NOW I WILL DO THE REST OF THE STUFF\n+YOU'VE CHOSEN "+N+" volume ");
 
+
   }
 
-  private double dlin;
-  private double shir;
-  private double visota;
-  String name;
   double N;
 
+  // private double dlin;
+  // private double shir;
+  // private double visota;
+
+  // private ArrayList<String> names = new ArrayList<String>();
+  // private ArrayList<Double> dlina = new ArrayList<Double>();
+  // private ArrayList<Double> shirina = new ArrayList<Double>();
+  // private ArrayList<Double> visotka = new ArrayList<Double>();
+
+  private ArrayList<String> names;
+  private ArrayList<Double> dlina;
+  private ArrayList<Double> shirina;
+  private ArrayList<Double> visotka;
+
   void main(double dlin , double shir, double visota){
-    this.dlin=dlin;
-    this.shir=shir;
-    this.visota=visota;
     System.out.println("высота -> "+ visota + " длина -> "+dlin+" ширина -> "+shir + "Имя фигуры - "+ name);
   }
 
@@ -30,6 +39,36 @@ class Main {
     System.out.println("радиус - "+ rad + " высота - " + visota);
   }
 
+// ТУТ НАЧИНАЕТСЯ ЖОПА. ЧИТАЙТЕ GET и SET или гляньте видос https://www.youtube.com/watch?v=wF18uVxjtvc&ab_channel=mybringback      XD
+
+/**
+ * @param dlina the dlina to set
+ */
+public void setDlina(ArrayList<Double> dlina) {
+  this.dlina = dlina;
+}
+
+/**
+ * @param names the names to set
+ */
+public void setNames(ArrayList<String> names) {
+  this.names = names;
+}
+
+/**
+ * @param shirina the shirina to set
+ */
+public void setShirina(ArrayList<Double> shirina) {
+  this.shirina = shirina;
+}
+
+/**
+ * @param visotka the visotka to set
+ */
+public void setVisotka(ArrayList<Double> visotka) {
+  this.visotka = visotka;
+}
+  
 }
 
 
@@ -47,10 +86,23 @@ abstract class scar extends Main{
 // ЧЕ , НЕ РАБОТАЕТ КЛАСС ?) А ОН И НЕ БУДЕТ РОБИТЬ , ИБО ОН АБСТРАКТНЫЙ, ЛМАО
 
 class scag extends Main{
-  public scag (int a){
+  public scag (){
     Scanner sc= new Scanner(System.in);
     double N= sc.nextInt();
     main(N);
   }
+
+  public ArrayList Figures(){
+    int N=(super.N);
+    String name;
+    Scanner sc= new Scanner(System.in);
+    for (int i=0; i < N; i++){
+      System.out.println("ENTER THE NAME OF THE FIGURE");
+      name= sc.nextInt();
+      //ArrayList<Double> X = new ArrayList<Double>();
+    }
+    
+  }
+
 }
 
