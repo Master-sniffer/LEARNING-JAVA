12c19033834c7e3734ab2fccc9bc544fe92d85d9
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-21 03:12:00 +0300

    Update Main.java

--- UNIVERSITY TASKS/HW TASKS/Second/Third_And_Fourth_TASK/Main.java
+++ UNIVERSITY TASKS/HW TASKS/Second/Third_And_Fourth_TASK/Main.java
@@ -1,19 +1,27 @@
 import java.util.*;
 import java.util.ArrayList; 
+import java.lang.Math.*;
 
 class Main {
 
   public static void main(String[] args) {
     System.out.println("WELCOME , AGAIN NIBBROID <*-*>\n");
-    new scag();
+    scag SC = new scag();
+    SC.Figures();
   }
-  
+
+
   public void main(double N){
     this.N=N;
     System.out.println("FUNNY , NOW I WILL DO THE REST OF THE STUFF\n+YOU'VE CHOSEN "+N+" volume ");
 
-
   }
+
+  public void mana(){
+
+    System.out.println(dlina);
+  }
+
 
   double N;
 
@@ -26,12 +34,13 @@ class Main {
   // private ArrayList<Double> shirina = new ArrayList<Double>();
   // private ArrayList<Double> visotka = new ArrayList<Double>();
 
-  private ArrayList<String> names;
-  private ArrayList<Double> dlina;
-  private ArrayList<Double> shirina;
-  private ArrayList<Double> visotka;
+  private  ArrayList<String> names;
+  private  ArrayList<Double> dlina;
+  private  ArrayList<Double> shirina;
+  private  ArrayList<Double> visotka;
 
-  void main(double dlin , double shir, double visota){
+
+  void main(double dlin , double shir, double visota, String name){
     System.out.println("объем -> "+ visota + " длина -> "+dlin+" ширина -> "+shir + "Имя фигуры - "+ name);
   }
 
@@ -72,6 +81,7 @@ public void setVisotka(ArrayList<Double> visotka) {
 }
 
 
+
 // НЕ ПОВЕРИТЕ, НО МОЖЕТ СУЩЕСТВОВАТЬ АБСТРАКТНЫЙ КЛАСС БЕЗ АБСТРАКТНЫХ МЕТОДОВ , ЧТО КАК БЫ ПОКАЗЫВАЕТ , ЧТО ВЫ НЕ ДРУЖИТЕ С ГОЛОВОЙ , НО ДА ПО...
 abstract class scar extends Main{ 
     public abstract void UNICHTOGIT_MIR(); // КАК ГОВОРИЛ МОЙ ДЕД, ЕСЛИ В ТВОЕМ АБСТРАКТНОМ КЛАССЕ НЕТ АБСТРАКТНЫХ МЕТОДОВ , ТО ХЕРНЯ ЭТО , А НЕ АБСТРАКТНЫЙ КЛАСС
@@ -83,7 +93,9 @@ abstract class scar extends Main{
   main(N);
   }
 }
-// ЧЕ , НЕ РАБОТАЕТ КЛАСС ?) А ОН И НЕ БУДЕТ РОБИТЬ , ИБО ОН АБСТРАКТНЫЙ, ЛМАО
+// ЧЕ , ПРОБЛЕМЫ С ДОСТУПОМ К  КЛАССУ ?) А ОН И НЕ БУДЕТ РОБИТЬ , ИБО ОН АБСТРАКТНЫЙ, ЛМАО
+
+
 
 class scag extends Main{
   public scag (){
@@ -92,12 +104,10 @@ class scag extends Main{
     main(N);
   }
 
-  public ArrayList Figures(){
-    int N=(super.N);
+  public void Figures(){
+    double N=(super.N);
 
-    
-
-    String name;
+    //String name;
     int x;
     Scanner sc= new Scanner(System.in);
     ArrayList<Double> dlin = new ArrayList<Double>();
@@ -108,31 +118,52 @@ class scag extends Main{
     ArrayList<Double> vaa = new ArrayList<Double>();
 
     for (int i=0; i < N; i++){
-      System.out.println("ENTER THE NAME OF THE FIGURE");
-      name= sc.nextInt();
+      System.out.println("\nENTER THE NAME OF THE FIGURE\n");
+      String name= sc.nextLine();
       namm.add(name);
-      System.out.println("GREAT !\nNOW ENTER THE TYPE OF THE FIGURE\n1- TRIANGLE\n2-CIRCLE\n3-SQUARE SHIET");
+      System.out.println("\nGREAT !\nNOW ENTER THE TYPE OF THE FIGURE\n1- TRIANGLE\n2-CIRCLE\n3-SQUARE SHIET\n");
+      x=0;
       x= sc.nextInt();
       if (x==1){
-        double dli;
-        double shi;
-        double vi;
-        vaa=new Triangle();
-        dli= vaa.get(0);
-        shi=vaa.get(1);
-        
+        // double dli;
+        // double shi;
+        // double vi;
+        Triangle vas=new Triangle();
+        vaa=vas.Triangles();
+
+        dlin.add(vaa.get(0));
+        shir.add(vaa.get(1));
+        vis.add(vaa.get(2));
+      }
+      else if (x==2){
+        Circle vas=new Circle();
+        vaa=vas.Circles();
+        dlin.add(vaa.get(0));
+        shir.add(vaa.get(1));
+        vis.add(vaa.get(2));
+      }
+      else if (x==3){
+        Square vas=new Square();
+        vaa=vas.Squares();
+        dlin.add(vaa.get(0));
+        shir.add(vaa.get(1));
+        vis.add(vaa.get(2));
+      }
+      else {
+        System.exit(1);
       }
     }
     
+    mana();
+    
   }
-
 }
 
-class Triangle extends scag {
-  public ArrayList Triangle(){
+class Triangle extends Main {
+  public ArrayList Triangles(){
     ArrayList<Double> vars = new ArrayList<Double>();
     Scanner sc= new Scanner(System.in);
-    System.out.println("WELL, THIS ONE WILL BE HARD... TYPE IN THE LENGTH OF UR FIGURE\n");
+    System.out.println("\nWELL, THIS ONE WILL BE HARD... TYPE IN THE LENGTH OF UR FIGURE\n");
     double dli= sc.nextInt();
     vars.add(dli);
     System.out.println("WELL,NOW ENTER HEIGHT\n");
@@ -143,6 +174,39 @@ class Triangle extends scag {
     
     return vars;
   }
-
 } 
+
+class Circle extends Main{
+  public ArrayList Circles(){
+    ArrayList<Double> vars = new ArrayList<Double>();
+    Scanner sc= new Scanner(System.in);
+    System.out.println("\nMMMM, CIRCLES\nMMMM\nAH,OH, ENTER THE RADIUS OF THE CIRCLE !\n");
+    double dli= sc.nextInt();
+    vars.add (dli);
+    vars.add(0.0);
+    double shi = 4/3 * Math.PI * Math.pow(dli,3);
+    vars.add(shi);
+
+    return vars;
+  }
+}
+
+class Square extends Main{
+  public ArrayList Squares(){
+    ArrayList<Double> vars = new ArrayList<Double>();
+    Scanner sc= new Scanner(System.in);
+    System.out.println("\nYEAH ! SQUARE\nENTER THE LENGTH, PLS\n");
+    double le = sc.nextInt();
+    System.out.println("\nYEAH ! SQUARE\nENTER THE WIDTH, PLS\n");
+    double shi = sc.nextInt();
+    System.out.println("\nYEAH ! SQUARE\nENTER THE HEIGHT, PLS\n");
+    double hi = sc.nextInt();
+    vars.add (le);
+    vars.add (shi);
+    hi = le*shi*hi;
+    vars.add (hi);
+    
+    return vars;
+  }
+}
 
