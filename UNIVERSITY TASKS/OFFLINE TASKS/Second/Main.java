bca2e7cd27d95bdd33f4a9aae73ffc64409e4e99
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-28 15:34:47 +0300

    Update Main.java

--- UNIVERSITY TASKS/OFFLINE TASKS/Second/Main.java
+++ UNIVERSITY TASKS/OFFLINE TASKS/Second/Main.java
@@ -22,44 +22,58 @@ class Main {
   }
 
   public static void MakingArray(double r, double X, double step){
-    ArrayList<Double> VSELENNAIA = new ArrayList<Double>();
-    VSELENNAIA.add(X);
+    
+    
     double buffer;
     double SecondBuffer;
     double end = 500.0/step;
+    
     System.out.println("The size of our array\n"+end+"\n");
+
+    for (double dr=0.1; dr < r; dr+=0.1){
+      ArrayList<Double> VSELENNAIA = new ArrayList<Double>();
+      VSELENNAIA.add(X);
+
     for (int i =0; i < end; i++){
+
       buffer=VSELENNAIA.get(i);
-      SecondBuffer= r* buffer*(1-buffer);
+      SecondBuffer= dr* buffer*(1.0-buffer);
+      // TODO ДОБАВИТЬ ДЛИНУ ДО 3 ЦИФР ПОСЛЕ ТОЧКИ
+      // ПРОВЕРИТЬ, ПОЧЕМУ ЧИСЛА ОТРИЦАТЕЛЬНЫЕ
       VSELENNAIA.add(SecondBuffer);
+
   }
-  System.out.println(VSELENNAIA);
+    System.out.println(VSELENNAIA+"\n\n\n\nHERE!!!!\t\n\n\n"+dr+"\n\n");
 
-  try(FileWriter writer = new FileWriter("TEKST.txt", false)) // ПРОБУЕМ ЗАПИСАТЬ ЭТО В ФАЙЛ
-        {
+    
 
-
-           // запись всей строки
-              //String text = "Hello Gold!";
-              //writer.write(text);
-            // запись по символам
-              //writer.append('\n');
-            //writer.append('E');
+    try(FileWriter writer = new FileWriter("TEKST.txt", true)) // ПРОБУЕМ ЗАПИСАТЬ ЭТО В ФАЙЛ
+          {
 
 
-            double var;
-            String s;
-            for (int i =0 ;i <VSELENNAIA.size(); i++){
-              var=VSELENNAIA.get(i);
-              s=String.valueOf(var)+"\n"; // достаем нашу переменную и делаем ее STRING-овым форматом , так как текстовый файл принимает ток такой формат
-              writer.append(s);
-            }
-             
-            writer.flush(); // закрываем файл
-        }
-        catch(IOException ex){
-             
-            System.out.println(ex.getMessage());
-        } 
+            // запись всей строки
+                //String text = "Hello Gold!";
+                //writer.write(text);
+              // запись по символам
+                //writer.append('\n');
+              //writer.append('E');
+
+
+              double var;
+              String s;
+              for (int i =0 ;i <VSELENNAIA.size(); i++){
+                var=VSELENNAIA.get(i);
+                s=String.valueOf(var)+"\n"; // достаем нашу переменную и делаем ее STRING-овым форматом , так как текстовый файл принимает ток такой формат
+                writer.append(s);
+              }
+                writer.append("hi\n");
+              
+              writer.flush(); // закрываем файл
+          }
+          catch(IOException ex){
+              
+              System.out.println(ex.getMessage());
+          } 
+     } // КОНЕЦ FOR
   }
 }
