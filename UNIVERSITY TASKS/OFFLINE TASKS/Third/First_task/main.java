49d6f1ab229cc16768dc392f4fb99647584089a8
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-10-19 14:40:58 +0300

    Create main.java

---
+++ UNIVERSITY TASKS/OFFLINE TASKS/Third/First_task/main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//Scanner sc= new Scanner(System.in);
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Vektor a= new Vektor();
+    System.out.println(a.getVLen(1,2,3));
+  }
+}
+
+
+
