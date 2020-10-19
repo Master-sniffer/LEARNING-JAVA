927c0031d0ac6e016d73ad6dd34ab5e26bd78156
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-10-19 15:27:25 +0300

    Update OverVektor.java

--- UNIVERSITY TASKS/OFFLINE TASKS/Third/First_task/OverVektor.java
+++ UNIVERSITY TASKS/OFFLINE TASKS/Third/First_task/OverVektor.java
@@ -1,6 +1,6 @@
 public interface OverVektor{
   double getVLen (int ... value);
-  
+  vektor getVSum (Vektor v);
   //double getVLen (int x, int y, int z);
   //getVSum (int x, int y, int z);
   //double getVMul (vektor);
