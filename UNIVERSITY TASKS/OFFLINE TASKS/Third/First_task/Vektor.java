d4261a0c98001d259f756bbb8a8914a8e844befc
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-10-19 15:27:42 +0300

    Update Vektor.java

--- UNIVERSITY TASKS/OFFLINE TASKS/Third/First_task/Vektor.java
+++ UNIVERSITY TASKS/OFFLINE TASKS/Third/First_task/Vektor.java
@@ -1,5 +1,28 @@
 public class Vektor implements OverVektor {
   
+  private int x;
+  private int y;
+  private int z;
+
+  public Vektor (int x ,int y ){
+    this.x=x;
+    this.y=y;
+  }
+  
+  public int getX() {
+    return x;
+   }
+
+  public int getY() {
+    return y;
+   }
+
+  public int getZ() {
+    return z;
+   }
+
+
+
   @Override
   public double getVLen (int ... value){
     double leng=0;
@@ -12,6 +35,16 @@ public class Vektor implements OverVektor {
     }
 
     return leng;
+  }
+
+  @Override
+  public Vektor getVSum(Vektor v){
+    double sum=0;
+    int x1=x+v.x;
+    int y1=y+v.y;
+    int z1=y+v.z;
+    return new Vektor (x1,y1,z1);
+    
   }
 
 }
