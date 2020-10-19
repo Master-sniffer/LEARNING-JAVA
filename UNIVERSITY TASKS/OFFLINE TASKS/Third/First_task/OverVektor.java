c54fd7913064f58cc831a5a5fd915164a1bf45de
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-10-19 14:42:10 +0300

    Create OverVektor.java

---
+++ UNIVERSITY TASKS/OFFLINE TASKS/Third/First_task/OverVektor.java
@@ -0,0 +1,9 @@
+public interface OverVektor{
+  double getVLen (int ... value);
+  
+  //double getVLen (int x, int y, int z);
+  //getVSum (int x, int y, int z);
+  //double getVMul (vektor);
+}
+
+
