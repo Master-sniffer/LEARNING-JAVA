470b8c97dfd698ac03a43602088a5db6cd3a593e
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-10-19 15:28:04 +0300

    Update main.java

--- UNIVERSITY TASKS/OFFLINE TASKS/Third/First_task/main.java
+++ UNIVERSITY TASKS/OFFLINE TASKS/Third/First_task/main.java
@@ -5,10 +5,9 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    Vektor a= new Vektor();
+    Vektor a= new Vektor(1,2,3);
     System.out.println(a.getVLen(1,2,3));
+    ra = a.getVSum(new Vektor (4,5,6));
+    System.out.println(a.getX() + a.getY()+ a.getZ())
   }
 }
-
-
-
