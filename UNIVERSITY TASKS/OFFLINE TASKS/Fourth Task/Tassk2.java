6cac670a873229ff479a0ecc986e9727d507420b
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-11-09 19:49:09 +0300

    Create Tassk2.java

---
+++ UNIVERSITY TASKS/OFFLINE TASKS/Fourth Task/Tassk2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner; // импорт сканера
+import java.util.concurrent.ThreadLocalRandom;
+
+class Tassk2 {
+  public static void Tassk2(ArrayList lis) {
+    System.out.println("Usual method is...");
+    long startTime = System.currentTimeMillis();
+
+    long endTime = System.currentTimeMillis();
+    System.out.println("Total execution time: " + (endTime-startTime) + "ms"); 
+
+  }
+}
