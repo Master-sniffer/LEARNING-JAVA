52439c9abdce202fcd85cb0eee688936fb23af44
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-11-10 13:15:48 +0300

    Create Task3.java

---
+++ UNIVERSITY TASKS/OFFLINE TASKS/Fourth Task/Task3.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner; // импорт сканера
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.IntConsumer;
+import javax.swing.JFrame;
+
+
+class Task3 {
+
+  Task3(){
+    int min=0;
+    double max=10.0;
+    double var=0;
+
+    ArrayList<Double> Numbers = new ArrayList<Double>();
+
+    for (int i=0 ; i<max; i+=0.001){
+      var = Math.cos(Math.pow(i, 5)) + Math.pow(i,4) -345.3 * i - 23 ;
+      System.out.println(var);
+      Numbers.add(var);
+    }
+
+  }
+}
