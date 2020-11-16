d9c69d2c121f6a464e5c74df334750143e5ab789
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-11-16 14:21:00 +0300

    Update Main.java

--- UNIVERSITY TASKS/OFFLINE TASKS/Fourth Task/Main.java
+++ UNIVERSITY TASKS/OFFLINE TASKS/Fourth Task/Main.java
@@ -21,6 +21,21 @@ class Main {
     }
     Tassk2 task2= new Tassk2(Numbers);
     Task3 task3 = new Task3();
+  //Создадим дерево с корневым элементом 33
+  System.out.println("Создадим дерево с корневым элементом 33");
+  Tree<Integer> tree = new Tree<>(33, null);
+  tree.add(5, 35, 1, 20, 4, 17, 31, 99, 18, 19);
+  //Распечатаем элементы дерева
+  tree.print();
+  //Удалим корень
+  tree.remove(33);
+  tree.remove(17);
+  tree.print();
+  //Проверяем элементы дерева
+  System.out.println(tree);
+  System.out.println(tree.left());
+  System.out.println(tree.left().left());
+  System.out.println(tree.right().left());
 
     
 
