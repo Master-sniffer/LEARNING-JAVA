95efda0c2a029ba04bd5b4bf3c726d145e933a32
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2021-03-08 20:24:55 +0300

    Add files via upload

--- UNIVERSITY TASKS/Cursach/Cursach1/Test Connection/src/sample/Main.java
+++ UNIVERSITY TASKS/Cursach/Cursach1/Test Connection/src/sample/Main.java
@@ -9,6 +9,16 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
+// THESE ARE NEEDED TO PARSE
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
 public class Main{
 
     public static void main(String[] args) throws IOException {
@@ -33,12 +43,48 @@ public class Main{
 
 
         // Second downloads "GET" Request from the site
-        URL website = new URL("https://db.chgk.info/xml/search/questions/types1/%D0%9F%D1%83%D1%88%D0%BA%D0%B8%D0%BD/A");
+        URL website = new URL("https://db.chgk.info/xml/random");
         ReadableByteChannel rbc = Channels.newChannel(website.openStream());
         FileOutputStream fos = new FileOutputStream("information.xml");
         fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
 
         // Now let's parse the file
-        
+
+        try {
+
+            File fXmlFile = new File("information.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("question");
+
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    // System.out.println("Staff id : " + eElement.getAttribute("QuestionId")); // WE USE THIS ONE IF WE HAVE SMTH LIKE <staff id="1001">
+                    System.out.println("Question : " + eElement.getElementsByTagName("Question").item(0).getTextContent());
+                    System.out.println("Answer : " + eElement.getElementsByTagName("Answer").item(0).getTextContent());
+
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
